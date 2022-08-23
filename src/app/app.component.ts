9f044070112bb12b3094bc6c00276678acdf9190
vijeeshvariyath <vijeeshvariyath0@gmail.com>
2022-08-23 16:01:59 +0530

    clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'clockproject';
+export class AppComponent implements OnInit{
+  second = 0;
+  minute = 0;
+  hour = 0;
+  d: Date | undefined; 
+
+  ngOnInit(){
+    setInterval(()=>{
+
+
+    this.d=new Date();
+    this.second=this.d.getSeconds()*6;
+    this.minute=this.d.getMinutes()*6;
+    this.hour=this.d.getHours()*30+Math.round(this.minute/12)
+  },1000)
+  }
 }
